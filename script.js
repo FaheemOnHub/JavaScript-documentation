9ea2073d9c60f42f1aa0b548ad4d186c4dfd9a89
FaheemOnHub <faheemmushtaq89@gmail.com>
2023-10-06 15:41:12 +0530

    Challenge-2 completed

--- script.js
+++ script.js
@@ -119,3 +119,78 @@ console.log(jessica.__proto__ == PersonCl.prototype); //same as a constructor fu
 
 //Setters set the value
 //Getters get the value
+
+const account = {
+  owner: 'Faheem',
+  movements: [200, 100, 200, 400],
+
+  get newData() {
+    return this.movements;
+  },
+
+  set newData(data) {
+    this.movements.push(data);
+  },
+};
+
+account.newData = 50;
+
+console.log(account.movements);
+
+//Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2023 - this.birthYear);
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+//Coding-Challenge #2
+//1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
+//2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide
+//by 1.6)
+//3. Add a setter called 'speedUS' which sets the current speed in mi/h (but
+//converts it to km/h before storing the value, by multiplying the input by 1.6)
+//4. Create a new car and experiment with the 'accelerate' and 'brake'
+//methods, and with the getter and setter.
+//Test data:
+//§ Data car 1: 'Ford' going at 120 km/h
+//GOOD LUCK �
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed}`);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed}`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(data) {
+    this.speed = data * 1.6;
+  }
+}
+
+const Ford = new CarCl('Ford', 120);
+Ford.accelerate();
+Ford.brake();
+
+Ford.speedUS = 50;
+console.log(Ford);
+console.log(Ford.speedUS);
